41b136dd9a678f78bbbc9618bfe83827ca8aa378
patrick <patrick.brito_ext@teladochealth.com>
2026-04-24 16:58:52 -0300

    ui(status-bar): force white status bar icons for better visibility over dark gradient

--- app/src/main/java/com/insight/launcher/MainActivity.kt
+++ app/src/main/java/com/insight/launcher/MainActivity.kt
@@ -8,8 +8,6 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.BitmapFactory
 import android.graphics.Color
-import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -28,29 +26,24 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.DecodeFormat
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.request.transition.Transition
-import com.facebook.shimmer.ShimmerFrameLayout
-import com.insight.launcher.data.ImageCacheManager
-import com.insight.launcher.data.repository.AppRepositoryImpl
-import com.insight.launcher.domain.usecase.GetInstalledAppsUseCase
-import com.insight.launcher.presentation.MainViewModel
-import com.insight.launcher.presentation.MainViewModelFactory
-import com.insight.launcher.presentation.model.AppUiModel
+    import androidx.recyclerview.widget.RecyclerView
+    import com.bumptech.glide.Glide
+    import com.bumptech.glide.load.DecodeFormat
+    import com.bumptech.glide.load.engine.DiskCacheStrategy
+    import com.bumptech.glide.request.RequestOptions
+    import com.facebook.shimmer.ShimmerFrameLayout
+    import com.insight.launcher.data.ImageCacheManager
+    import com.insight.launcher.data.repository.AppRepositoryImpl
+    import com.insight.launcher.domain.usecase.GetInstalledAppsUseCase
+    import com.insight.launcher.presentation.MainViewModel
+    import com.insight.launcher.presentation.MainViewModelFactory
+    import com.insight.launcher.presentation.model.AppUiModel
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var backgroundImageView: ImageView
     private lateinit var shimmerContainer: ShimmerFrameLayout
@@ -95,6 +88,8 @@ class MainActivity : AppCompatActivity() {
         try {
             enableEdgeToEdge()
             setContentView(R.layout.activity_main)
+
+            WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
 
             recyclerView = findViewById(R.id.recyclerView)
             backgroundImageView = findViewById(R.id.backgroundImageView)
